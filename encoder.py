f7ca07c8cdc7a77a64a3623cbe8acd1a5efb01ce
bchowlur <bchowlur@ufl.edu>
2024-10-21 20:09:15 -0400

    main and encoder file

---
+++ encoder.py
@@ -0,0 +1,9 @@
+def encode(password):
+    encoded_password = ""
+    for digit in password:
+        new_digit = (int(digit) + 3) % 10
+        encoded_password += str(new_digit)
+    return encoded_password
+
+def decode(encoded_password):
+    pass
